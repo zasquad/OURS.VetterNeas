5759a87d709d8020643ffb4054995dc9610254eb
ZVETTER001 <zvetter001@vplincsci005.ad.csbsju.edu>
2018-03-21 14:47:57 -0500

    Eclipse exercise

---
+++ src/edu/csbsju/cs/SimplePrint.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package edu.csbsju.cs;
+
+/**
+ * @author zvetter001
+ *
+ */
+public class SimplePrint {
+
+	private String pName;
+	private int pAge;
+	
+	/**
+	 * @param pName
+	 * @param pAge
+	 */
+	public SimplePrint(String pName, int pAge) {
+		super();
+		this.pName = pName;
+		this.pAge = pAge;
+	}
+
+	/**
+	 * @return the pName
+	 */
+	public String getpName() {
+		return pName;
+	}
+
+	/**
+	 * @param pName the pName to set
+	 */
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	/**
+	 * @return the pAge
+	 */
+	public int getpAge() {
+		return pAge;
+	}
+
+	/**
+	 * @param pAge the pAge to set
+	 */
+	public void setpAge(int pAge) {
+		this.pAge = pAge;
+	}
+
+	/**
+	 * 
+	 */
+	public SimplePrint() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<args.length;i++) {
+		System.out.println("args[ "+i+" ] = " + args[i]);
+		}
+	}
+
+}
